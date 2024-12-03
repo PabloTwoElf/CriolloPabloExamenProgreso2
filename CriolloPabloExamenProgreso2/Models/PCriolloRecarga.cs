23b727b82ee64c9ab93a35216bf4ab103df7d134
PabloTwoElf <pablo.escoba2005@gmail.com>
2024-12-03 07:33:08 -0500

    Creacion de models, PCriolloGridLayout, y imagenes de iconos

---
+++ CriolloPabloExamenProgreso2/Models/PCriolloRecarga.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriolloPabloExamenProgreso2.Models
+{
+    public class PCriolloRecarga
+    {
+        public string Nombre { get; set; }
+
+        public string Numero{ get; set; }
+
+    }
+}
---
+++ CriolloPabloExamenProgreso2/PCriolloGridLayout.xaml.cs
@@ -0,0 +1,9 @@
+namespace CriolloPabloExamenProgreso2;
+
+public partial class PCriolloGridLayout : ContentPage
+{
+	public PCriolloGridLayout()
+	{
+		InitializeComponent();
+	}
+}
