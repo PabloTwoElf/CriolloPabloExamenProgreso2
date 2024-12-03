b0a11858383c1882a194f5b73adf50161585dc7d
PabloTwoElf <pablo.escoba2005@gmail.com>
2024-12-03 08:04:40 -0500

    Creacion de la interfaz

---
+++ CriolloPabloExamenProgreso2/Interfaces/ICriolloRecarga.cs
@@ -0,0 +1,20 @@
+﻿using CriolloPabloExamenProgreso2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriolloPabloExamenProgreso2.Interfaces
+{
+    internal interface ICriolloRecarga
+    {
+        PCriolloRecarga GetRecarga(string numero);
+        bool CreateRecarga(ICriolloRecarga recarga);
+        bool UpdateRecarga(ICriolloRecarga recarga);
+        bool DeleteRecarga(string numero);
+
+
+
+    }
+}
