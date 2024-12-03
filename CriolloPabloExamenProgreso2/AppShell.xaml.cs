3fa7163c419b875d19bd58f4f2f539c6385405d6
PabloTwoElf <pablo.escoba2005@gmail.com>
2024-12-03 07:19:50 -0500

    Agregar archivos de proyecto.

---
+++ CriolloPabloExamenProgreso2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CriolloPabloExamenProgreso2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CriolloPabloExamenProgreso2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CriolloPabloExamenProgreso2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
